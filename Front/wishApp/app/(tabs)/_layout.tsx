bc99a78e5db327fea15783e6489bfc98f65293f8
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2025-09-23 14:40:10 +0200

    style: footer  + nav

--- Front/wishApp/app/(tabs)/_layout.tsx
+++ Front/wishApp/app/(tabs)/_layout.tsx
@@ -1,12 +1,11 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -17,7 +16,13 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
+        tabBarBackground: () => (
+          <LinearGradient
+            colors={["#FF4DA6", "#A64DFF"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={{ flex: 1 }}
+          />),
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
@@ -29,22 +34,59 @@ export default function TabLayout() {
       <Tabs.Screen
         name="gift/index"
         options={{
+          headerShown: true,
+          headerTitle: 'Mes Cadeaux',
           title: '',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="gift" size={35} color={color} />,
+          headerBackground: () => (
+            <LinearGradient
+              colors={["#FF4DA6", "#A64DFF"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={{ flex: 1 }}
+            />
+          ),
+          headerTitleStyle: {
+            color: "white", fontSize: 25, fontWeight: 'bold'
+          }, 
+          headerTitleAlign: "center",
+          tabBarIcon: ({ focused }) => (<View style={{ backgroundColor: focused ? "white" : "transparent", borderRadius: 50, padding: focused ? 10 : 0,}}>
+            <MaterialCommunityIcons name="gift" size={focused ? 50 : 35} color={"#000000ff"}/></View>),
         }}
       />
       <Tabs.Screen
         name="wishList/index"
         options={{
+          headerShown: true,
+          headerTitle: 'Mes Wishlists',
           title: '',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="clipboard-check" size={35} color={color} />,
+          headerBackground: () => (
+            <LinearGradient
+              colors={["#FF4DA6", "#A64DFF"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={{ flex: 1 }}
+            />
+          ),
+          headerTitleStyle: {
+            color: "white", fontSize: 25, fontWeight: 'bold'
+          },  
+          headerTitleAlign: "center",
+          tabBarIcon: ({focused }) => (<View style={{ backgroundColor: focused ? "white" : "transparent", borderRadius: 50, padding: focused ? 10 : 0,}}>
+            <MaterialCommunityIcons name="clipboard-check" size={focused ? 50 : 35} color={"#000000ff"}/></View>),
         }}
       />
       <Tabs.Screen
         name="account/index"
         options={{
+          headerShown: true,
+          headerTitle: 'Mon Profil',
           title: '',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account" size={35} color={color}/>,
+          headerTitleStyle: {
+            color: "#B266FF", fontSize: 25, fontWeight: 'bold'
+          },  
+          headerTitleAlign: "center",
+          tabBarIcon: ({ focused }) => (<View style={{ backgroundColor: focused ? "white" : "transparent", borderRadius: 50, padding: focused ? 10 : 0,}}>
+            <MaterialCommunityIcons name="account" size={focused ? 50 : 35} color={"#000000ff"}/></View>),
         }}
       />
     </Tabs>
