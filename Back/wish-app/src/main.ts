55556b3b01eaef872a5f685b316747d7b34b031d
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2025-11-21 09:49:28 +0100

    maj back

--- Back/wish-app/src/main.ts
+++ Back/wish-app/src/main.ts
@@ -5,9 +5,10 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8082',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: '*'
   });
 
   // Récupérer Reflector depuis le container
